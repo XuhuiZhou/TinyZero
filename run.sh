b11b0b39b93491b557784c5d02f9efe19b4e8557
Xuhui Zhou <zhouxuhui2018@gmail.com>
2025-02-13 23:12:25 -0500

    fix bug

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 export N_GPUS=2
 export BASE_MODEL=./models/Qwen2.5-3B
-export DATA_DIR=/usr2/xuhuiz/data/countdown
+export DATA_DIR=./data/countdown
 export ROLLOUT_TP_SIZE=2
 export EXPERIMENT_NAME=countdown-qwen2.5-3b
 export VLLM_ATTENTION_BACKEND=XFORMERS
