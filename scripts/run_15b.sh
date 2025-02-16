c36ad1b9b696a4ed9bdd13398b52b6e39a893707
Xuhui Zhou <zhouxuhui2018@gmail.com>
2025-02-16 11:41:12 -0500

    remove wandb api

--- scripts/run_15b.sh
+++ scripts/run_15b.sh
@@ -4,6 +4,5 @@ export DATA_DIR=./data
 export ROLLOUT_TP_SIZE=2
 export EXPERIMENT_NAME=countdown-qwen2.5-1.5b
 export VLLM_ATTENTION_BACKEND=XFORMERS
-export WANDB_API_KEY=4f58c216e8b5d314ea2bab9409546321eaa683ef
 
 bash ./scripts/train_tiny_zero.sh
