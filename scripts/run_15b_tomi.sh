f342b78c03aadead1524f01630ae0d0a081c899c
Xuhui Zhou <zhouxuhui2018@gmail.com>
2025-02-16 20:10:06 -0500

    fix tomi

--- scripts/run_15b_tomi.sh
+++ scripts/run_15b_tomi.sh
@@ -1,6 +1,6 @@
 export N_GPUS=2
 export BASE_MODEL=./models/Qwen2.5-1.5B
-export DATA_DIR=./data
+export DATA_DIR=./data/tomi_data
 export ROLLOUT_TP_SIZE=2
 export EXPERIMENT_NAME=tomi-qwen2.5-1.5b
 export VLLM_ATTENTION_BACKEND=XFORMERS
